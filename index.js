581a126b47f956ca41d1d2e541437e515bf4c8eb
vj-abishek <abigosearch@gmail.com>
2021-01-13 19:29:15 +0530

    Add prettier eslint husky

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const main = async () => {
-    try {
-        const { ACCESS_TOKEN } = process.env;
-        if (!ACCESS_TOKEN) {
-            return core.setFailed('ENV required and not supplied: ACCESS_TOKEN');
-        }
-
-        const octokit = github.getOctokit(ACCESS_TOKEN);
-
-        const { payload } = github.context;
-        const inviteeId = payload.issue.user.id;
-        const currentLabel = payload.label.name;
-
-        const org = core.getInput('organization', { required: true });
-        const label = core.getInput('label', { required: true });
-
-        if (currentLabel === label) {
-            try {
-                await octokit.orgs.checkMembership({
-                    org,
-                    username: payload.issue.user.login,
-                });
-            } catch (error) {
-                await octokit.orgs.createInvitation({
-                    org,
-                    invitee_id: inviteeId,
-                });
-            }
-        }
-    } catch (error) {
-        return core.setFailed(error.message);
+  try {
+    const { ACCESS_TOKEN } = process.env;
+    if (!ACCESS_TOKEN) {
+      return core.setFailed('ENV required and not supplied: ACCESS_TOKEN');
     }
-    return core.setOutput('Invitation sent successfully 🎉');
+
+    const octokit = github.getOctokit(ACCESS_TOKEN);
+
+    const { payload } = github.context;
+    const inviteeId = payload.issue.user.id;
+    const currentLabel = payload.label.name;
+
+    const org = core.getInput('organization', { required: true });
+    const label = core.getInput('label', { required: true });
+
+    if (currentLabel === label) {
+      try {
+        await octokit.orgs.checkMembership({
+          org,
+          username: payload.issue.user.login,
+        });
+      } catch (error) {
+        await octokit.orgs.createInvitation({
+          org,
+          invitee_id: inviteeId,
+        });
+      }
+    }
+  } catch (error) {
+    return core.setFailed(error.message);
+  }
+  return core.setOutput('Invitation sent successfully 🎉');
 };
 main();
